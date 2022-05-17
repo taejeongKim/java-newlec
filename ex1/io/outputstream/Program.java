bc3f74133d9e1330368a3d8b90355b0432d12c02
taejeongKim <dolgaemi@naver.com>
2022-05-17 18:51:53 +0900

    git start

---
+++ ex1/io/outputstream/Program.java
@@ -0,0 +1,18 @@
+package ex1.io.outputstream;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		// '3'은 문자3에 해당하는 문자표를 반환한다.
+		System.out.write('3'+1); 
+		System.out.write('3'+2);
+		System.out.write('3'+3);
+		System.out.write('3'+4);
+		
+		// outputstream 버퍼에 담긴 것을 내보낸다.
+		System.out.flush();  
+		
+	}
+
+}
---
+++ test/Test.java
@@ -0,0 +1,12 @@
+package test;
+
+import java.util.Scanner;
+
+public class Test {
+	public static void main(String[] args) {
+
+		int x;
+		x = 4;
+		System.out.print(2 < x && x < 10);
+	}
+}
